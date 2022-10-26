84930753a912212ce8af08238c7a06c62db4300a
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-26 15:38:09 +0600

    resolve cors

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express();
-// const cors = require('cors')
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-// app.use(cors())
+app.use(cors())
 
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
     res.send('Welcome to Educational hour server')
+})
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
 })
 
 app.listen(port, () => {
